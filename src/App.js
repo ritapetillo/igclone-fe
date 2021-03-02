1efa6dd3681d0994061554e79c3c5c129e1eeb22
Manuel Desole <desole.manuel@gmail.com>
2021-03-02 12:18:54 +0100

    starting sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Route } from "react-router-dom";
 import "./App.scss";
 import ProfilePicture from "./Components/ProfilePic";
+
+//*Styles
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//*Components
 import Login from "./Pages/Login";
 
 function App() {
