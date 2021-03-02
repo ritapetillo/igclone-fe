a5f5c46540e82cb45f17a1d82cd3e7c4a5b6afdf
LidiaKovac <lidia.kovac1998@gmail.com>
2021-03-02 21:14:01 +0100

    adds scrap for navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route } from "react-router-dom";
 import "./App.scss";
+import Navbar from "./Components/Navbar/Navbar";
 import SinglePost from "./Components/SinglePost/SinglePost";
 import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={SinglePost} />
+      <Route path="/" component={Navbar} />
+      <Route path="/" component={SinglePost}/>
     </div>
   );
 }
