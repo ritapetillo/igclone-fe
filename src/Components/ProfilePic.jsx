f98697a153b9b4d6b13b247455952c3788587e9e
LidiaKovac <lidia.kovac1998@gmail.com>
2021-03-02 12:02:14 +0100

    adds shapes

--- src/Components/ProfilePic.jsx
+++ src/Components/ProfilePic.jsx
@@ -5,8 +5,12 @@ const ProfilePicture = () => {
     return (<><div className="story-lg">
         <img src="http://placehold.it/150x150" className="circle-lg"/>
     </div>
-    <div className='circle-md' style={{background: "red"}}></div>
-    <div className='circle-sm' style={{background: "red"}}></div>
+    <div className="story-md">
+        <img src="http://placehold.it/150x150" className="circle-md"/>
+    </div><div className="story-sm">
+        <img src="http://placehold.it/150x150" className="circle-sm"/>
+    </div>
+    
     </>
     )
 }
