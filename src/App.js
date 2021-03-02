8402f652329c89444aef659ae4c58151c48ce8a7
LidiaKovac <lidia.kovac1998@gmail.com>
2021-03-02 12:18:15 +0100

    adds assets, fonts and spinner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route } from "react-router-dom";
 import "./App.scss";
-import ProfilePicture from "./Components/ProfilePic";
+import Spinner from "./Components/Spinner/Spinner"
 import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={ProfilePicture} />
+      <Route path="/" component={Spinner} />
     </div>
   );
 }
---
+++ src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Spinner.scss"
+
+const Spinner = () => {
+    return (<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>)
+}
+
+export default Spinner
