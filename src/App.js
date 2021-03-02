ef36664a085a0e45c8328be5bbf0b01c9233e7bf
Manuel Desole <desole.manuel@gmail.com>
2021-03-02 15:55:30 +0100

    sidebar static completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { Route } from "react-router-dom";
 import "./App.scss";
-import ProfilePicture from "./Components/ProfilePic";
-
-//*Styles
-import "bootstrap/dist/css/bootstrap.min.css";
-
-//*Components
+// import Spinner from "./Components/Spinner/Spinner";
 import Login from "./Pages/Login";
+
+import Feed from "./Pages/Feed";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={ProfilePicture} />
+      {/* <Route path="/" component={ProfilePicture} /> */}
+      {/* <Route path="/spinner" component={Spinner} /> */}
+      <Route path="/feed" component={Feed} />
     </div>
   );
 }
