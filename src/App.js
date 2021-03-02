821bfacf909d2655c70dc49bde8e1c802a650ff6
Manuel Desole <desole.manuel@gmail.com>
2021-03-02 16:35:21 +0100

    Revert "Merge branch 'develop' into sidebar"
    This reverts commit 96147818487fdc5e6a92f08906553684be45c625, reversing
    changes made to ef36664a085a0e45c8328be5bbf0b01c9233e7bf.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Route } from "react-router-dom";
 import "./App.scss";
-
-import Spinner from "./Components/Spinner/Spinner"
-
+// import Spinner from "./Components/Spinner/Spinner";
 import Login from "./Pages/Login";
 
 import Feed from "./Pages/Feed";
@@ -12,12 +10,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-
-    
+      {/* <Route path="/" component={ProfilePicture} /> */}
+      {/* <Route path="/spinner" component={Spinner} /> */}
       <Route path="/feed" component={Feed} />
-
-      <Route path="/" component={Spinner} />
-
     </div>
   );
 }
