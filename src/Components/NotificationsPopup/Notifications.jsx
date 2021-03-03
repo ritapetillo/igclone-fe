d2fb10821f7bf6db117f4ea1a96e129b015c8ec7
LidiaKovac <lidia.kovac1998@gmail.com>
2021-03-03 10:42:15 +0100

    adds some notification work

--- src/Components/NotificationsPopup/Notifications.jsx
+++ src/Components/NotificationsPopup/Notifications.jsx
@@ -1,17 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Notifications.scss"
-import {IoHeartOutline} from "react-icons/io5"
-import Spinner from "../Spinner"
+import { IoHeartOutline } from "react-icons/io5"
+import Spinner from "../Spinner/Spinner"
+import "../../Styling/Shapes.scss"
 
-const Notifications = (props) => {
+const Notifications = () => {
+    const [show, setShow] = useState(false)
     return (
-    <div className="dropdown-wrap">
-        <IoHeartOutline className="nav-icon" />
-        <div className="dropdown-content">
-           <Spinner/> 
+        <div className="dropdown-wrap">
+            <IoHeartOutline className="nav-icon" onClick={() => setShow(!show)} />
+            <div className={show ? "dropdown-content-show" : "dropdown-hidden"}>
+                    {/*<Spinner className="notification-spinner"/>*/}
+                    <span className="notification-group-date">Today: </span> 
+                    <div className="notification-core">
+                        <img src="https://i.pravatar.cc/100" className="circle-notification"/>
+                    </div>
+                    <div className="notification-divider"></div>
+                    <span className="notification-group-date">Yesterday: </span> 
+                
+            </div>
+            <div className={show ? "dropdown-arrow-show" : "dropdown-hidden"}></div>
         </div>
-        <div className="dropdown-arrow"></div>
-  </div>
     )
 }
 
