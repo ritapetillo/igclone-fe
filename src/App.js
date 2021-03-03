d555a1a599a39a2c9e27421e472415c9175dc97c
LidiaKovac <lidia.kovac1998@gmail.com>
2021-03-03 09:14:55 +0100

    morning commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Navbar from "./Components/Navbar/Navbar";
 import SinglePost from "./Components/SinglePost/SinglePost";
 import Login from "./Pages/Login";
 
+import Feed from "./Pages/feed/feed";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
