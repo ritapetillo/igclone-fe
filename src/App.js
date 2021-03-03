520d9e437beedd335e220e49f89311f30fa72c52
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-03-03 05:20:24 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { useDispatch } from "react-redux";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Navbar from "./Components/Navbar/Navbar";
 // import Spinner from "./Components/Spinner/Spinner";
 import Login from "./Pages/Login";
 import SocketTest from "./Pages/SocketTest";
-
 import Feed from "./Pages/Feed/Feeds";
-
+import SinglePost from "./Components/SinglePost/SinglePost";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const dispatch = useDispatch();
   return (
     <div className="App">
-      <Route exact path="/feed" component={Feed} />
-      <Route path="/" component={Navbar} />
-      <Route path="/" component={SinglePost} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/socket" component={SocketTest} />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <div>
+          <Navbar />
+          <Route exact path="/feed" component={Feed} />
+          <Route exact path="/socket" component={SocketTest} />
+        </div>
+      </Switch>
     </div>
   );
 }
