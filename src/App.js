52cdf530057624f255c2a08a084d230662d812ba
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-03-03 03:55:15 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import "./App.scss";
 import Login from "./Pages/Login";
 import SocketTest from "./Pages/SocketTest";
 
+<<<<<<< Updated upstream
+=======
+import Feed from "./Pages/feed";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+>>>>>>> Stashed changes
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <Route path="/login" component={Login} />
       <Route path="/socket" component={SocketTest} />
+=======
+      <Route exact path="/feed" component={Feed} />
+      <Route path="/" component={Navbar} />
+      <Route path="/" component={SinglePost} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/socket" component={SocketTest} />
+>>>>>>> Stashed changes
     </div>
   );
 }
