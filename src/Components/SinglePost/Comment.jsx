d769cd446b711fad8340e2d4c4de983462a8c0e3
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2021-03-05 11:14:03 +0000

    ediitng comments attempt

--- src/Components/SinglePost/Comment.jsx
+++ src/Components/SinglePost/Comment.jsx
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { IoHeartOutline, IoHeartSharp } from "react-icons/io5";
-import { useSelector } from "react-redux";
+import { IoHeartOutline, IoHeartSharp, IoPencil } from "react-icons/io5";
+import { useSelector, useDispatch } from "react-redux";
 import { likeAComment, unlikeAComment } from "../../Api/commentApi";
-
-const Comment = ({ comment }) => {
+import {editCommentAction} from "../../Actions/commentActions"
+const Comment = ({ comment, postId }) => {
   const [liked, setLiked] = useState(false);
-  const currentUser = useSelector(
-    (state) => state.currentUser.user.currentUser
-  );
+  const [editMode, setEditMode] = useState(false);
+
+  const currentUser = useSelector(state => state.currentUser.user.currentUser);
+const dispatch = useDispatch();
+  // const comment = useRef();
+
+  const handleEdit = async event => {
+    const editedComment = {
+      text: event.target.value,
+      postId: postId,
+    };
+    if (event.key === "Enter") {
+      console.log("editedComment", editedComment);
+      dispatch(editCommentAction(editedComment, postId));
+    }
+    setEditMode(false);
+  };
   useEffect(() => {
     // when the component mounts, you want to check if the current user is within the array of liked from the comment
     //if that's the case, it means that current user liked that comment
@@ -17,18 +31,24 @@ const Comment = ({ comment }) => {
 
   const handleLike = async () => {
     //we send the request to the backed. If the status is liked, we will dislike the comment and viceversa
-    //we will use the state of like to understand which API request we need to make. 
+    //we will use the state of like to understand which API request we need to make.
     if (!liked) await likeAComment(comment._id);
     else await unlikeAComment(comment._id);
     setLiked(!liked);
   };
   return (
     <div className="single-comment">
-      <div className="comment-infos">
-        <div className="comment-author">{comment.author}</div>
-        <div className="comment-text">{comment.text}</div>
-      </div>
-
+      {!editMode ? (
+        <>
+        <div className="comment-infos">
+          <div className="comment-author">{comment.author}</div>
+          <div className="comment-text">{comment.text}</div>
+        </div>
+        <IoPencil onCLick ={()=>setEditMode(true)} />
+        </>
+      ) : (
+        <input type="text" id="text" value={comment.text} />
+      )}
       {!liked ? (
         <IoHeartOutline
           className="comment-add-like"
