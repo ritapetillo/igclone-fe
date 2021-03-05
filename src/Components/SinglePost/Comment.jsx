9349a09f938aa97c39f8f1d495763d7844d08cff
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2021-03-05 11:47:04 +0000

    deleting updates working

--- src/Components/SinglePost/Comment.jsx
+++ src/Components/SinglePost/Comment.jsx
@@ -1,32 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { IoHeartOutline, IoHeartSharp, IoPencil } from "react-icons/io5";
+import {
+  IoHeartOutline,
+  IoHeartSharp,
+  IoPencil,
+  IoTrashBinOutline,
+} from "react-icons/io5";
 import { useSelector, useDispatch } from "react-redux";
 import { likeAComment, unlikeAComment } from "../../Api/commentApi";
-import {editCommentAction} from "../../Actions/commentActions"
+import {
+  editCommentAction,
+  deleteCommentAction,
+} from "../../Actions/commentActions";
 const Comment = ({ comment, postId }) => {
   const [liked, setLiked] = useState(false);
   const [editMode, setEditMode] = useState(false);
+  const [editComment, setEditComment] = useState("");
 
   const currentUser = useSelector(state => state.currentUser.user.currentUser);
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
   // const comment = useRef();
 
   const handleEdit = async event => {
-    const editedComment = {
-      text: event.target.value,
-      postId: postId,
-    };
-    if (event.key === "Enter") {
-      console.log("editedComment", editedComment);
-      dispatch(editCommentAction(editedComment, postId));
-    }
-    setEditMode(false);
+    setEditComment({ ...editComment, text: event.target.value });
+    console.log("editComment", editComment);
   };
+
+  const submitEditComment = async event => {
+    if (event.key === "Enter") {
+      await dispatch(editCommentAction(editComment, postId));
+      setEditMode(false);
+    }
+    console.log(event);
+  };
+
   useEffect(() => {
     // when the component mounts, you want to check if the current user is within the array of liked from the comment
     //if that's the case, it means that current user liked that comment
     //then we want to set liked to true
     setLiked(comment.likes.includes(currentUser.username));
+    setEditComment(comment);
   }, []);
 
   const handleLike = async () => {
@@ -40,14 +52,27 @@ const dispatch = useDispatch();
     <div className="single-comment">
       {!editMode ? (
         <>
-        <div className="comment-infos">
-          <div className="comment-author">{comment.author}</div>
-          <div className="comment-text">{comment.text}</div>
-        </div>
-        <IoPencil onCLick ={()=>setEditMode(true)} />
+          <div className="comment-infos">
+            <div className="comment-author">{comment.author}</div>
+            <div className="comment-text">{comment.text}</div>
+          </div>
+          {currentUser.username === editComment.author && (
+            <IoPencil onClick={() => setEditMode(true)} />
+          )}
+          {currentUser.username === editComment.author && (
+            <IoTrashBinOutline
+              onClick={() => dispatch(deleteCommentAction(comment, postId))}
+            />
+          )}
         </>
       ) : (
-        <input type="text" id="text" value={comment.text} />
+        <input
+          type="text"
+          id="text"
+          value={editComment && editComment.text}
+          onChange={handleEdit}
+          onKeyDown={submitEditComment}
+        />
       )}
       {!liked ? (
         <IoHeartOutline
